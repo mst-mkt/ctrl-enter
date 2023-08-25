4da9d1dd8c88576ae8c86ccd963b4d054094e45c
mst-mkt <s1f102303057@iniad.org>
2023-08-25 19:43:01 +0900

    fix: lint修正

--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,21 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState('')
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
+        display: 'flex',
+        flexDirection: 'column',
         padding: 16
       }}>
       <h2>
         Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
+        <a href="https://www.plasmo.com"> Plasmo</a> Extension!
       </h2>
       <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      <a href="https://docs.plasmo.com">View Docs</a>
     </div>
   )
 }
