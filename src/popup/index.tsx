d3d9dae4d9299c117fbaaae33cb5cbbb0237785d
mst-mkt <s1f102303057@iniad.org>
2023-08-27 00:33:00 +0900

    add: strageを試しに使用してみる

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,15 +1,42 @@
 import { IconSettings } from '@tabler/icons-react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type ChangeEvent } from 'react'
+
+import { useStorage } from '@plasmohq/storage/hook'
 
 import styles from './index.module.css'
 
 export const IndexPopup = () => {
   const [url, setUrl] = useState<string | null>(null)
+  const [excludedUrls, setExcludedUrls] = useStorage<string[]>(
+    'excludedUrls',
+    []
+  )
   const [isChecked, setIsChecked] = useState<boolean>()
 
-  const check = (e: InputEvent) => {
+  const check = (e: ChangeEvent<HTMLInputElement>) => {
+    if (url === null) {
+      return
+    }
     setIsChecked((e.target as HTMLInputElement).checked)
+    setExcludedUrls((prev) => {
+      if (isChecked ?? false) {
+        return prev?.filter((u) => u !== url) ?? []
+      }
+      return [...(prev ?? []), url]
+    })
   }
+
+  const openSettings = () => {
+    chrome.runtime.openOptionsPage()
+  }
+
+  useEffect(() => {
+    if (url === null) {
+      setIsChecked(false)
+      return
+    }
+    setIsChecked(excludedUrls.includes(url))
+  }, [url, excludedUrls])
 
   useEffect(() => {
     const getUrl = () => {
@@ -23,10 +50,6 @@ export const IndexPopup = () => {
       getUrl()
     })
   }, [])
-
-  const openSettings = () => {
-    chrome.runtime.openOptionsPage()
-  }
 
   if (url === null) {
     return <div className={styles.container}>loading...</div>
@@ -43,7 +66,12 @@ export const IndexPopup = () => {
       <main>
         <div>
           <label htmlFor="checkbox">現在のurl({url})を除外</label>
-          <input type="checkbox" id="checkbox" checked={isChecked} />
+          <input
+            type="checkbox"
+            id="checkbox"
+            checked={isChecked}
+            onChange={(e) => check(e)}
+          />
         </div>
       </main>
     </div>
