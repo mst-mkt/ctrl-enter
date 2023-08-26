5456fd3c463a3b2483e0215353b9f70117fd4b02
mst-mkt <s1f102303057@iniad.org>
2023-08-27 00:13:51 +0900

    add: 仮のpopupを追加

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,21 +1,51 @@
-import { useState } from 'react'
+import { IconSettings } from '@tabler/icons-react'
+import { useEffect, useState } from 'react'
 
 import styles from './index.module.css'
 
 export const IndexPopup = () => {
-  const [data, setData] = useState<string[]>([])
+  const [url, setUrl] = useState<string | null>(null)
+  const [isChecked, setIsChecked] = useState<boolean>()
 
-  const addData = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setData([...data, e.target.value])
+  const check = (e: InputEvent) => {
+    setIsChecked((e.target as HTMLInputElement).checked)
+  }
+
+  useEffect(() => {
+    const getUrl = () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        setUrl(tabs[0].url ?? null)
+      })
+    }
+
+    getUrl()
+    chrome.tabs.onActivated.addListener((activeInfo) => {
+      getUrl()
+    })
+  }, [])
+
+  const openSettings = () => {
+    chrome.runtime.openOptionsPage()
+  }
+
+  if (url === null) {
+    return <div className={styles.container}>loading...</div>
   }
 
   return (
     <div className={styles.container}>
-      <h2>除外サイト登録</h2>
-      <input onChange={(e) => addData(e)} value={data} />
-      <p>
-        詳細設定は<a href="https://docs.plasmo.com">こちら</a>から
-      </p>
+      <header className={styles.header}>
+        <h1 className={styles.title}>ctrl + Enter</h1>
+        <div className={styles.icon} onClick={openSettings}>
+          <IconSettings />
+        </div>
+      </header>
+      <main>
+        <div>
+          <label htmlFor="checkbox">現在のurl({url})を除外</label>
+          <input type="checkbox" id="checkbox" checked={isChecked} />
+        </div>
+      </main>
     </div>
   )
 }
