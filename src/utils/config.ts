f94ad8e5bb6f075e9e16f19a60c3577444cf319e
mst-mkt <s1f102303057@iniad.org>
2023-09-04 23:52:37 +0900

    fix: コンフィグを改良

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,28 @@
-import type { keyActions, keys } from 'src/types/type'
+import { Storage } from '@plasmohq/storage'
 
-export const keyConfig: Record<keys, keyActions> = {
-  enter: 'newLine',
-  ctrlEnter: 'send',
-  shiftEnter: 'none',
-  altEnter: 'none'
+const storage: Storage = new Storage()
+
+const defaultConfig: Record<string, boolean> = {
+  discord: true,
+  twitter: true,
+  instagram: true,
+  chatgpt: true,
+  bing: true,
+  bard: true
+} as const
+
+export const supportSites: Array<keyof typeof defaultConfig> =
+  Object.keys(defaultConfig)
+
+export const getConfig = async (): Promise<Record<string, boolean>> => {
+  const configString = await storage.get('config')
+
+  if (configString === undefined) return defaultConfig
+
+  const config = JSON.parse(configString)
+  return config ?? defaultConfig
+}
+
+export const saveConfig = async (config: Record<string, boolean>) => {
+  await storage.set('config', JSON.stringify(config))
 }
