a909bc77353cebdf062c7939cef1a0eb4a2fbd0e
imoken777 <s1f102301640@iniad.org>
2024-05-13 19:04:41 +0900

    add:機能面claude対応

---
+++ src/contents/claude.ts
@@ -0,0 +1,66 @@
+import type { PlasmoCSConfig } from 'plasmo'
+import { getConfig } from 'src/utils/config'
+import { key } from 'src/utils/key'
+
+export const config: PlasmoCSConfig = {
+  matches: ['https://claude.ai/chat/*', 'https://claude.ai/chats'],
+  all_frames: true
+}
+
+const sendButton = {
+  send: (elm: HTMLElement) => {
+    const parent = elm.closest('.flex.items-start.gap-4')
+    const buttonAriaLabel = 'Send Message'
+    const button = parent?.querySelector(
+      `button[aria-label="${buttonAriaLabel}"]`
+    ) as HTMLButtonElement | undefined
+    return button
+  },
+  startChat: (elm: HTMLElement) => {
+    const parent = elm.closest('.grid')
+    const button = parent?.querySelector('button.w-full') as
+      | HTMLButtonElement
+      | undefined
+    return button
+  }
+}
+
+const handleKeyEvent = (e: KeyboardEvent) => {
+  if (isTextArea(e)) {
+    e.stopPropagation()
+    if (key(e) === 'ctrlEnter') {
+      const target = e.target as HTMLElement
+
+      const currentUrl = window.location.href
+      const action = currentUrl.includes('/chat/') ? 'send' : 'startChat'
+
+      const button = sendButton[action](target)
+      button?.click()
+    }
+  }
+}
+
+const isTextArea = (e: KeyboardEvent) => {
+  const target = e.target as HTMLElement
+  return (
+    target.className ===
+    'ProseMirror break-words max-w-[60ch] ProseMirror-focused'
+  )
+}
+
+const handleEvent = async () => {
+  const config = await getConfig()
+  const claudeConfig = config.claude
+
+  if (claudeConfig) {
+    document.addEventListener('keydown', handleKeyEvent, { capture: true })
+  } else {
+    document.removeEventListener('keydown', handleKeyEvent, { capture: true })
+  }
+}
+
+chrome.storage.onChanged.addListener(() => {
+  handleEvent()
+})
+
+handleEvent()
